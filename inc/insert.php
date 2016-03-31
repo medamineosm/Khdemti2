2c96271fbbfdb8f1306269db9e83b895bfc581d5
Fati <elmediani@gmail.com>
2016-03-31 13:50:46 +0100

    commit fz

--- inc/insert.php
+++ inc/insert.php
@@ -14,19 +14,19 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 
 
 if($db->insertPerson(
-	$_POST['nom']['$modelValue'], 
-	$_POST['email']['$modelValue'], 
-	$_POST['tel']['$modelValue'], 
-	$_POST['age']['$modelValue'], 
-	$_POST['ville']['$modelValue'], 
+	$_POST['nom']['$modelValue'],
+	$_POST['email']['$modelValue'],
+	$_POST['tel']['$modelValue'],
+	$_POST['age']['$modelValue'],
+	$_POST['ville']['$modelValue'],
 	$_POST['emploi']['$modelValue'],
 	$_POST['prenom']['$modelValue']))
 {
-    $data['message'] = 'We will notified you soon as possible !';
+    $data['message'] = 'الموقع في طور اللانجاز , سنعلمك بكل جديد عن الموقع في اقرب وقت';
     echo json_encode($data);
 
 }else{
-    $data['message'] = 'You are already registed !';
+    $data['message'] = 'انت مسجل مسبقا';
     echo json_encode($data);
 }
 ?>
